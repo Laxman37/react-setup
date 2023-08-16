5a41537947e8110194c39efd5059eb598c9bced9
laxman37 <laxman@qxlabai.com>
2023-08-16 12:46:20 +0530

    files modified

---
+++ src/AnimationExamples/Bounce.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
+import "./Bounce.module.css";
+
+const Bounce = () => {
+  return (
+    <div class="cont">
+      <button class="btn">
+        <span>Submit</span>
+        <img
+          src="https://i.cloudup.com/2ZAX3hVsBE-3000x3000.png"
+          height="62"
+          width="62"
+        />
+      </button>
+    </div>
+  );
+};
+
+export default Bounce;
