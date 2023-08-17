f8a02013e3b8dd2d85f1119503dd231eec316945
laxman37 <laxman@qxlabai.com>
2023-08-17 11:07:48 +0530

    aded for flex and grid layout design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,58 @@
-
 function App() {
   return (
-    <div className="App">
-     <h1 style={{textAlign:"center"}}>Hello Devlopers! here we go we have project setup 🚀 </h1>
+    <div className="App" style={{ padding: "20px" }}>
+      <h1 style={{ textAlign: "center" }}>
+        Hello Devlopers! here we go we have project setup 🚀
+      </h1>
+
+      <div
+        className="flex-container"
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3, 1fr)",
+          gridGap: 20,
+        }}
+      >
+        <div
+          className="flex-item"
+          style={{
+            background: "red",
+            padding: "40px",
+          }}
+        >
+          hello
+        </div>
+        <div
+          className="flex-item"
+          style={{
+            background: "yellow",
+
+            padding: "40px",
+          }}
+        >
+          hello
+        </div>
+        <div
+          className="flex-item"
+          style={{
+            background: "orange",
+
+            padding: "40px",
+          }}
+        >
+          hello
+        </div>
+        <div
+          className="flex-item"
+          style={{
+            background: "orange",
+
+            padding: "40px",
+          }}
+        >
+          hello
+        </div>
+      </div>
     </div>
   );
 }
