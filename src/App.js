43637cdd13e3352ffcada89ed9472edcf4bfe546
laxman37 <laxman@qxlabai.com>
2023-09-07 11:01:15 +0530

    added the one more rocket emoji

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-
 function App() {
   return (
     <div className="App">
-     <h1 style={{textAlign:"center"}}>Hello Devlopers! here we go we have project setup 🚀 </h1>
+      <h1 style={{ textAlign: "center" }}>
+        Hello Devlopers! here we go we have project setup 🚀 🚀
+      </h1>
     </div>
   );
 }
